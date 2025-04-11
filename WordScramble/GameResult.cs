6eafb8e7e49fff4e5276f1f28586256fef28ebe7
Magali <magamayrmata@gmail.com>
2025-04-11 17:13:12 +0100

    trying again

--- WordScramble/GameResult.cs
+++ WordScramble/GameResult.cs
@@ -34,7 +34,9 @@ namespace WordScramble
         /// <see cref="double"/> (in seconds).</param>
         public GameResult(string word, double timeTaken)
         {
-            GameResult = GameResult;
+            Word = word;
+            TimeTaken = timeTaken;
+
         }
     }
 }
