0fb4f2a0edb15629260e4935062c969be6bb5a06
Magali <magamayrmata@gmail.com>
2025-04-11 17:36:07 +0100

    table fixed

--- WordScramble/Game.cs
+++ WordScramble/Game.cs
@@ -166,6 +166,11 @@ namespace WordScramble
                     (i + 1).ToString(),
                     gameStats[i].Word,
                     gameStats[i].TimeTaken.ToString("F2"));
+                
+                AnsiConsole.Write(table);
+                AnsiConsole.Markup(
+                    "\n[bold green]Press Enter to Return to the Menu...[/]");
+                Console.ReadLine();
             }
         }
     }
